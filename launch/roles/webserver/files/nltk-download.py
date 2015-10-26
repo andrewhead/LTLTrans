5090ec2ef745ae6c1f905e9abf4af99b5cfa9f90
Andrew Head <head.andrewm@gmail.com>
2015-10-25 17:23:17 -0700

    Launch: Cleaned up rules for launching on a new machine

---
+++ launch/roles/webserver/files/nltk-download.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nltk
+
+nltk.download('maxent_treebank_pos_tagger', download_dir='/usr/local/share/nltk_data')
+nltk.download('punkt', download_dir='/usr/local/share/nltk_data')
+nltk.download('wordnet', download_dir='/usr/local/share/nltk_data')
